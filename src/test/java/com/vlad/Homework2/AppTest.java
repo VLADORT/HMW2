ded22f5388ad05cf6cd3f96352f484481632ad0b
Vlad <vlad300820001@gmail.com>
2019-06-05 14:34:53 +0300

    initial commit

---
+++ src/test/java/com/vlad/Homework2/AppTest.java
@@ -0,0 +1,20 @@
+package com.vlad.Homework2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
